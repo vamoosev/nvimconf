2f9dc2de9b405c89710054de22675387b6b93149
vamoosev <vamoosev@gmail.com>
2023-06-06 15:09:14 +0300

    muutoksia on paljon

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,7 +4,8 @@ lsp.preset("recommended")
 
 lsp.ensure_installed({
   'rust_analyzer',
-  'pyright'
+  'pyright',
+  'asm_lsp'
 })
 
 -- Fix Undefined global 'vim'
--- lua/aagr/remap.lua
+++ lua/aagr/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
+vim.keymap.set("n", "<leader>sv", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>sh", vim.cmd.split)
+vim.keymap.set("n", "<leader>te", vim.cmd.terminal)
